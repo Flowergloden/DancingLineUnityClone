a3c04964a7881ea61224b458e11e288c64549050
13270 <1327097302@qq.com>
2022-09-09 21:17:04 +0800

    重新启用GameMode

--- Assets/Scrips/GameMode.cs
+++ Assets/Scrips/GameMode.cs
@@ -6,10 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class GameMode : MonoBehaviour
 {
+    public AudioSource bgm;
+    public void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Start"))
+        {
+            Time.timeScale = 1;
+            bgm.Play();
+        }
+    }
+
     public static void Lost()
     {
         Time.timeScale = 0;
-        if (Input.GetButtonDown("Turn"))
+        if (Input.GetButtonDown("Start"))
             SceneManager.LoadScene("Scenes/SampleScene");
     }
 }
